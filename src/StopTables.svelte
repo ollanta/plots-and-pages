059061fa0e37f0ac4d4dcc0f85938f2598deff96
Ollanta CG <ollantaster@gmail.com>
2021-03-11 21:53:19 +0100

    Remove unused slot

--- src/StopTables.svelte
+++ src/StopTables.svelte
@@ -18,9 +18,8 @@
 </script>
 
 {#each data.tables as table, idx (table.id)}
-  <StopTable bind:data={table.data} keys={data.keys}>
-    <button on:click="{() => removeTable(idx)}" disabled="{data.tables.length == 1}">-</button>
-  </StopTable>
+  <StopTable bind:data={table.data} keys={data.keys}/>
+  <button on:click="{() => removeTable(idx)}" disabled="{data.tables.length == 1}">-</button>
 {/each}
 <button on:click="{newTable}">+</button>
 <hr>
