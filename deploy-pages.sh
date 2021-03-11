ca9243a20e0aca8406baf6223a6070561be1351c
Ollanta CG <ollantaster@gmail.com>
2021-03-11 22:48:13 +0100

    Further deploy fixes

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -4,5 +4,7 @@ set -euxo pipefail
 npm run build
 git add -f public/build
 git commit -m "Build bundle"
+SPLITREF=$(git subtree split --prefix public/)
+git push -f origin $SPLITREF:gh-pages
 git subtree push --prefix public/ origin gh-pages
 git reset HEAD~1
